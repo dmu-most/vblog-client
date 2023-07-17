d291da520edbfd3ecef8509f3aceb68e36952472
HwanTech <78897615+wjdghksdigh@users.noreply.github.com>
2023-07-18 01:15:45 +0900

    Feat/footer (#8)
    * Image folder 추가 및 index.tsx 수정
    
    * footer 수정
    
    * design:footer UI 구성

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,5 +1,4 @@
 import { ReactNode } from 'react';
-import styled from 'styled-components';
 
 
 import Header from '@layout/Header';
@@ -10,11 +9,6 @@ type LayoutProps = {
   children: ReactNode;
 };
 
-// const LayoutContainer = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   min-height: 100vh;
-// `;
 
 const Layout = ({ children }: LayoutProps) => {
   return (
