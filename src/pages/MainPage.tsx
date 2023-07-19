3b57abe65c3eb750de1b27153ce9dc1a63f12656
HwanTech <78897615+wjdghksdigh@users.noreply.github.com>
2023-07-19 15:55:33 +0900

    feat: Mainpage 생성 및 /경로 생성 (#10)

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PostCard from "@components/common/PostCard";
+
+const MainPage = () => {
+    return (
+        <div>
+            <PostCard/>
+        </div>
+  );
+};
+
+export default MainPage;
