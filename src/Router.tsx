28ce42e4735cd8b0c54d42d985ba60020049ed7b
sineTlsl <limsieun0313@gmail.com>
2023-07-25 17:45:29 +0900

    feat: 회원가입 폼 레이아웃 구현

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,6 +4,7 @@ import Layout from '@layout/index';
 // pages
 import MainPage from '@pages/MainPage';
 import SignInPage from '@pages/SignInPage';
+import SignUpPage from '@pages/SignUpPage';
 
 const Router = () => {
   return (
@@ -11,6 +12,7 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/signin" element={<SignInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
       </Routes>
     </Layout>
   );
