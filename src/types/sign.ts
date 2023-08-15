83d0f19e28cd215cea8d1a983282391a9d77e32e
sineTlsl <limsieun0313@gmail.com>
2023-08-15 16:01:11 +0900

    feat: 회원가입 폼 유효성 검사 추가

---
+++ src/types/sign.ts
@@ -0,0 +1,8 @@
+/** 2023/08/15 - 회원가입 폼 양식 Type - sineTlsl */
+export interface SignUpFormType {
+  id: string;
+  email: string;
+  name: string;
+  password: string;
+  passwordConfirm: string;
+}
