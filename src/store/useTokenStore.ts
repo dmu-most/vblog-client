041033597447cea2dff641877145d44e8d933cd0
sineTlsl <limsieun0313@gmail.com>
2023-08-21 15:28:49 +0900

    feat: 토큰 저장소 구현

---
+++ src/store/useTokenStore.ts
@@ -0,0 +1,46 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
+interface Token {
+  accessToken: string;
+  refreshToken: string;
+  setAccessToken: (accessToken: string) => void;
+  setRefreshToken: (refreshToken: string) => void;
+  clearTokens: () => void;
+}
+
+type StateSetter = (partialState: Partial<Token>) => void;
+
+/** 2023/08/21 - 로컬스토리지에 액세스 토큰 저장 - by sineTlsl */
+const setAccessToken = (set: StateSetter) => (accessToken: string) => {
+  set({ accessToken });
+};
+
+/** 2023/08/21 - 로컬스토리지에 리프레시 토큰 저장 - by sineTlsl */
+const setRefreshToken = (set: StateSetter) => (refreshToken: string) => {
+  set({ refreshToken });
+};
+
+/** 2023/08/21 - 로컬스토리지에서 토큰 삭제 - by sineTlsl */
+const clearTokens = (set: StateSetter) => () => {
+  set({ accessToken: '', refreshToken: '' });
+};
+
+/** 2023/08/21 - 토큰 저장소 - by sineTlsl */
+const useTokenStorage = create(
+  persist<Token>(
+    set => ({
+      accessToken: '',
+      refreshToken: '',
+      setAccessToken: setAccessToken(set),
+      setRefreshToken: setRefreshToken(set),
+      clearTokens: clearTokens(set),
+    }),
+    {
+      name: 'tokenStorage',
+      getStorage: () => localStorage,
+    },
+  ),
+);
+
+export { useTokenStorage };
