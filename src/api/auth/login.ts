a134a49f7c51bc38f3b554dcf511e524cc14cbad
sineTlsl <limsieun0313@gmail.com>
2023-09-12 17:08:00 +0900

    feat: auth 폴더 index 파일 추가

--- src/api/auth/login.ts
+++ src/api/auth/login.ts
@@ -5,7 +5,11 @@ import { LoginFormType } from 'types/auth/login';
 
 /** 2023/09/09 - 로그인 POST 요청 - by sineTlsl */
 export const postLogin = async (body: LoginFormType) => {
-  const { data } = await instance.post('/login', body);
+  const { data, headers } = await instance.post('/login', body);
 
-  return data;
+  return {
+    data,
+    accessToken: headers['authorization'],
+    refreshToken: headers['refresh'],
+  };
 };
