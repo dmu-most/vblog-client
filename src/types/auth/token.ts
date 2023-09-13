8fc7088727aff66e331b90f94deef3ecd7212101
sineTlsl <limsieun0313@gmail.com>
2023-09-14 02:26:18 +0900

    feat: 유용한 토큰인지 확인 후, 서버에게 토큰 재발급 요청 구현

---
+++ src/types/auth/token.ts
@@ -0,0 +1,6 @@
+// ============================ 토큰 ============================
+/** 2023/09/13 - 액세스 토큰 검증 (Response) - by sineTlsl */
+export interface CheckAccessTokenResponse {
+  result: boolean;
+  message: string;
+}
