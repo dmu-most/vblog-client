563e29b5086d1138695be4b0bd3285bc79921ccc
sineTlsl <limsieun0313@gmail.com>
2023-09-20 18:17:36 +0900

    feat: OAuth 소셜 로그인 기능 추가

--- src/pages/OauthCallbackPage.tsx
+++ src/pages/OauthCallbackPage.tsx
@@ -1,3 +1,4 @@
+import { useMemberStore } from '@store/useMemberStore';
 import { useTokenStore } from '@store/useTokenStore';
 import { useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
@@ -5,22 +6,34 @@ import { useLocation, useNavigate } from 'react-router-dom';
 /** 2023/08/20 - OAuth 콜백 페이지 - by sineTlsl */
 const OauthCallbackPage: React.FC = (): JSX.Element => {
   const { setAccessToken, setRefreshToken } = useTokenStore();
+  const { setMember } = useMemberStore();
   const navigate = useNavigate();
   const location = useLocation();
 
   useEffect(() => {
     const queryParams = new URLSearchParams(location.search);
 
-    const accessToken = queryParams.get('access');
-    const refreshToken = queryParams.get('refresh');
+    const authorization = queryParams.get('Authorization');
+    const refreshTokenParam = queryParams.get('RefreshToken');
+    const imageUrl = queryParams.get('imageUrl');
+    const userName = queryParams.get('username');
+
+    // null인지 확인
+    const accessToken = authorization ? authorization.split(' ')[1] : null;
+    const refreshToken = refreshTokenParam ? refreshTokenParam.split(' ')[1] : null;
 
     if (accessToken && refreshToken) {
       setAccessToken(accessToken);
       setRefreshToken(refreshToken);
 
+      setMember({
+        imageUrl,
+        username: userName,
+      });
+
       navigate('/');
     }
-  }, []);
+  }, [location.search, setAccessToken, setRefreshToken, setMember]);
 
   return <div>Loading...</div>;
 };
