b944d1ce58e9dbc8c76109e4aec251b4a6bb5aa2
sineTlsl <limsieun0313@gmail.com>
2023-09-20 17:21:41 +0900

    feat: 디바이스 크기 수정

--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -4,7 +4,6 @@ import React, { useEffect } from 'react';
 // store
 import { useContentModeStore } from '@store/useConentModeStore';
 
-
 interface MainProps {
   className?: string;
   children: React.ReactNode;
@@ -15,7 +14,7 @@ const Main: React.FC<MainProps> = ({ className, children }) => {
 
   // Update the --bg-mode variable based on the mode
   useEffect(() => {
-    document.documentElement.style.setProperty('--bg-mode', mode === "V" ? "var(--bg-green)" : "var(--bg-brown)");
+    document.documentElement.style.setProperty('--bg-mode', mode === 'V' ? 'var(--bg-green)' : 'var(--bg-brown)');
   }, [mode]);
 
   return (
@@ -28,8 +27,24 @@ const Main: React.FC<MainProps> = ({ className, children }) => {
 export default Main;
 
 const MainContainer = styled.div`
-  max-width: ${({ theme }) => theme.widthSize.contentMax};
+  max-width: ${({ theme }) => theme.widthSize.contentMaxL}; // 1350px
   width: 100%;
   height: 100%;
   margin-bottom: 50px;
+
+  // 디바이스 크기가 1580px 클 경우 1500px
+  @media ${props => props.theme.breakpoints.desktopLMax} {
+    max-width: ${({ theme }) => theme.widthSize.contentMaxXL};
+  }
+
+  // 디바이스 크기가 1439px 보다 작을 경우 1250px
+  @media ${props => props.theme.breakpoints.desktopMMax} {
+    max-width: ${({ theme }) => theme.widthSize.contentMaxM};
+    padding: 0 2rem;
+  }
+
+  // 디바이스 크기가 1250px 보다 작을 경우 1150px
+  @media ${props => props.theme.breakpoints.desktopSMax} {
+    max-width: ${({ theme }) => theme.widthSize.contentMaxS};
+  }
 `;
