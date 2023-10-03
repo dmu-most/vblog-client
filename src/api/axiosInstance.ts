ffdea2feac8e395cbe353c34d53369cbba0a4cf4
sineTlsl <limsieun0313@gmail.com>
2023-10-03 23:02:44 +0900

    fix: axios-instance 수정

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -11,22 +11,19 @@ const instance = axios.create({
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
+    'ngrok-skip-browser-warning': '69420',
   },
 });
 
 /** 2023/08/21 - Request interceptor 설정 - by sineTlsl */
 instance.interceptors.request.use(
   async config => {
-    const { accessToken, refreshToken, setAccessToken } = getTokenState();
+    const { accessToken, refreshToken } = getTokenState();
 
     // 특별한 헤더를 삭제
     const noAuth = config.headers['No-Auth'];
     const onlyAuthorization = config.headers['Only-Authorization'];
     const onlyRefresh = config.headers['Only-Refresh'];
-
-    if (config.url && (config.url.includes('/token/refresh-access') || config.url.includes('/token/verify-access'))) {
-      return config;
-    }
 
     delete config.headers['No-Auth'];
     delete config.headers['Only-Authorization'];
@@ -37,35 +34,16 @@ instance.interceptors.request.use(
       return config;
     }
 
-    // 액세스 토큰만 필요할 때
-    if (onlyAuthorization) {
-      if (accessToken) {
-        config.headers['Authorization'] = `Bearer ${accessToken}`;
-      }
-    } else if (onlyRefresh) {
+    if (onlyAuthorization && accessToken) {
+      // 액세스 토큰만 필요할 때
+      config.headers['Authorization'] = `Bearer ${accessToken}`;
+    } else if (onlyRefresh && refreshToken) {
       // 리프레시 토큰만 필요할 때
-      if (refreshToken) {
-        config.headers['Refresh'] = `Bearer ${refreshToken}`;
-      }
+      config.headers['Refresh'] = `Bearer ${refreshToken}`;
     } else if (refreshToken && accessToken) {
       // 액세스 & 리프레시 토큰 둘 다 필요할 때
       config.headers['Authorization'] = `Bearer ${accessToken}`;
       config.headers['Refresh'] = `Bearer ${refreshToken}`;
-    }
-
-    try {
-      // 유효한 액세스 토큰인지 확인
-      console.log('Attempting to get Access Token >> ', accessToken);
-      const { result } = await getAccessToken();
-      console.log('너 지금 테스트하니..?', result);
-
-      // 유효하지 않을 경우 재발급
-      if (!result) {
-        const res = await postNewAccessToken();
-        setAccessToken(res.accessToken);
-      }
-    } catch (error) {
-      console.error('Error:', error);
     }
 
     return config;
@@ -78,11 +56,41 @@ instance.interceptors.request.use(
 /** 2023/08/21 - Response interceptor 설정 - by sineTlsl */
 instance.interceptors.response.use(
   res => {
-    // 응답 데이터로 작업 수행
-
     return res;
   },
-  err => {
+  async err => {
+    const originalRequest = err.config;
+    const { setAccessToken, setRefreshToken, refreshToken } = getTokenState();
+
+    // originalRequest._retry: 원래의 요청을 이미 한 번 다시 보냈는지를 나타내는 플래그
+    if (err.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      return axios
+        .post(
+          `${process.env.REACT_APP_API_URL}/token/refresh-access`,
+          {},
+          {
+            headers: {
+              refresh: refreshToken,
+            },
+          },
+        )
+        .then(res => {
+          if (res.status === 200) {
+            setAccessToken(res.headers.authorization);
+
+            console.log(res);
+            return instance(originalRequest);
+          }
+        })
+        .catch(() => {
+          console.log('다지워짐 헤헤');
+          setRefreshToken('');
+          setAccessToken('');
+        });
+    }
+
     return Promise.reject(err);
   },
 );
