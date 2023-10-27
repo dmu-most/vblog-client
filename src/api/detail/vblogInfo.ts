5c0156ed4bb6bf6f8f7123f788357571ed2be1c5
sineTlsl <limsieun0313@gmail.com>
2023-10-27 14:08:32 +0900

    fix: axios interceptors 액세스 토큰만 요청하는 걸 기본으로 지정

--- src/api/detail/vblogInfo.ts
+++ src/api/detail/vblogInfo.ts
@@ -7,9 +7,5 @@ import { VblogLikeRequest } from 'types/detail/vblog';
 
 /** 2023/10/13 - 브블로그 좋아요/싫어요 - by jh */
 export const PostLikeInfo = async (contentId: number, body: VblogLikeRequest) => {
-  return await instance.post(`/like/${contentId}`, body, {
-    headers: {
-      'Only-Authorization': true,
-    },
-  });
+  return await instance.post(`/like/${contentId}`, body);
 };
--- src/api/my-info/content.ts
+++ src/api/my-info/content.ts
@@ -11,37 +11,21 @@ import {
 // ============================ 스크랩 ============================
 /** 2023/10/23 - 스크랩 폴더 POST 요청 - by sineTlsl */
 export const postScrapFolder = async (name: string, type: string) => {
-  const { data } = await instance.post<ScrapResponseType>(
-    '/folders',
-    { name, type },
-    {
-      headers: {
-        'Only-Authorization': true,
-      },
-    },
-  );
+  const { data } = await instance.post<ScrapResponseType>('/folders', { name, type }, {});
 
   return data;
 };
 
 /** 2023/10/23 - 스크랩 폴더 Vlog GET 요청 - by sineTlsl */
 export const getScrapVlog = async () => {
-  const { data } = await instance.get<ScrapResponseType>('/myinfo/folders/vlog', {
-    headers: {
-      'Only-Authorization': true,
-    },
-  });
+  const { data } = await instance.get<ScrapResponseType>('/myinfo/folders/vlog');
 
   return data;
 };
 
 /** 2023/10/23 - 스크랩 폴더 Blog GET 요청 - by sineTlsl */
 export const getScrapBlog = async () => {
-  const { data } = await instance.get<ScrapResponseType>('/myinfo/folders/blog', {
-    headers: {
-      'Only-Authorization': true,
-    },
-  });
+  const { data } = await instance.get<ScrapResponseType>('/myinfo/folders/blog');
 
   return data;
 };
@@ -49,22 +33,14 @@ export const getScrapBlog = async () => {
 // ============================ 최근목록 ============================
 /** 2023/10/15 - 최근목록 Vlog GET 요청 - by sineTlsl */
 export const getRecentVlog = async () => {
-  const { data } = await instance.get<RecentContent[]>('/myinfo/recently/vlog', {
-    headers: {
-      'Only-Authorization': true,
-    },
-  });
+  const { data } = await instance.get<RecentContent[]>('/myinfo/recently/vlog');
 
   return data;
 };
 
 /** 2023/10/15 - 최근목록 Blog GET 요청 - by sineTlsl */
 export const getRecentBlog = async () => {
-  const { data } = await instance.get<RecentContent[]>('/myinfo/recently/blog', {
-    headers: {
-      'Only-Authorization': true,
-    },
-  });
+  const { data } = await instance.get<RecentContent[]>('/myinfo/recently/blog');
 
   return data;
 };
@@ -75,9 +51,6 @@ export const getMyReviewVlog = async (page: number) => {
   const { data } = await instance.get<ReviewResponseType>('/myinfo/reviews/vlog', {
     params: {
       page,
-    },
-    headers: {
-      'Only-Authorization': true,
     },
   });
 
@@ -90,9 +63,6 @@ export const getMyReviewBlog = async (page: number) => {
     params: {
       page,
     },
-    headers: {
-      'Only-Authorization': true,
-    },
   });
 
   return data;
@@ -100,22 +70,14 @@ export const getMyReviewBlog = async (page: number) => {
 
 /** 2023/10/23 - 리뷰 DELETE 요청 - by sineTlsl */
 export const deleteMyReview = async (reviewId: number) => {
-  const { data } = await instance.delete<CommonResponseType>(`/review/${reviewId}`, {
-    headers: {
-      'Only-Authorization': true,
-    },
-  });
+  const { data } = await instance.delete<CommonResponseType>(`/review/${reviewId}`);
 
   return data;
 };
 
 /** 2023/10/25 - 리뷰 PUT 요청 - by sineTlsl */
 export const putMyReview = async (reviewId: number, body: ReviewPutRequest) => {
-  const { data } = await instance.put<CommonResponseType>(`/review/${reviewId}`, body, {
-    headers: {
-      'Only-Authorization': true,
-    },
-  });
+  const { data } = await instance.put<CommonResponseType>(`/review/${reviewId}`, body, {});
 
   return data;
 };
